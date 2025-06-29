c71d3209ffa6e3f7927b650c31d98484752fc8c8
zj-zc003 <13051550590@163.com>
2025-06-29 08:59:11 +0800

    A

---
+++ src/main/java/com/example/usermanagercrud/UserManagerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermanagercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagerCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usermanagercrud/UserManagerCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermanagercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
